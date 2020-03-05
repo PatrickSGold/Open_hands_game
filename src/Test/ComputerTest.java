37ce46695c68d2a340ed5dfbd8f704fb9aa8e9ba
patrickgold <patrickgold89@outlook.com>
2020-03-05 18:12:05 +0700

    .

---
+++ src/Test/ComputerTest.java
@@ -0,0 +1,32 @@
+package Test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ComputerTest {
+
+    @Test
+    void computerAnswer() {
+    }
+
+    @Test
+    void countComputerOpenHands() {
+    }
+
+    @Test
+    void computerPredictionNumber() {
+    }
+
+    @Test
+    void setTurn() {
+    }
+
+    @Test
+    void setComputerAnswer() {
+    }
+
+    @Test
+    void getComputerAnswer() {
+    }
+}
---
+++ src/Test/UserTest.java
@@ -0,0 +1,40 @@
+package Test;
+
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Test;
+
+class UserTest {
+
+    @org.junit.jupiter.api.Test
+    void promptUserForInput() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void countUserOpenHands() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setTurn() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setUserAnswer() {
+    }
+
+    @Test
+    void getUserAnswer() {
+    }
+
+    @Test
+    void getUserPredictionNumber() {
+    }
+
+    @Test
+    void getIfUserIsPredictor() {
+    }
+}
